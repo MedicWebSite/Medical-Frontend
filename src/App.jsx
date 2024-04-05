9331d69542d2158b734504253f4572c9474c450e
“mehrojalieev” <aliyevmehrojbek8006@gmail.com>
2024-04-05 14:27:03 +0500

    Created project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import RouteController from './routes/RouteController';
+
+function App() {
+  return (
+    <div className="App">
+      <RouteController/>
+    </div>
+  );
+}
+
+export default App;

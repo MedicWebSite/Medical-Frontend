9f6fd806093fd31e2ae212180d5e16b89fd1b71b
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-04-09 11:01:55 +0500

    services section

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import './Services.scss'
+const Services = () => {
+    return (
+        <div className="services">
+            <div className="container">
+                <div className='services-sub'>
+                    <p className='services-sub-title'>WHAT ABOUT US</p>
+                    <h2 className='services-title'>The Skin And Science Of Test</h2>
+                </div>
+                <div className="service-cards">
+                    <div className="service-card">
+                        <div className="service-card-top">
+                            <div className="service-img"><i class="fa-solid fa-heart-pulse"></i></div>
+                            <h3>Anti Skin Care</h3>
+                        </div>
+                        <p>There are many variations of pas of Lorem Ipsum availab.There are many variations of pas of Lorem Ipsum availab.</p>
+                        <div className="service-box-list">
+                            <ul>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="service-card">
+                        <div className="service-card-top">
+                            <div className="service-img"><i class="fa-solid fa-x-ray"></i></div>
+                            <h3>Anti Skin Care</h3>
+                        </div>
+                        <p>There are many variations of pas of Lorem Ipsum availab.There are many variations of pas of Lorem Ipsum availab.</p>
+                        <div className="service-box-list">
+                            <ul>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="service-card">
+                        <div className="service-card-top">
+                            <div className="service-img"><i class="fa-solid fa-capsules"></i></div>
+                            <h3>Anti Skin Care</h3>
+                        </div>
+                        <p>There are many variations of pas of Lorem Ipsum availab.There are many variations of pas of Lorem Ipsum availab.</p>
+                        <div className="service-box-list">
+                            <ul>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                                <li><i class="fa-solid fa-check"></i> Endocrinology Services</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Services
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../../components/hero/Hero'
+import Services from '../../components/services/Services'
 
 const Home = () => {
   return (
     <div>
-      <Hero/>
-      <Hero/>
+      <Hero />
+      <Hero />
+      <Services />
     </div>
   )
 }

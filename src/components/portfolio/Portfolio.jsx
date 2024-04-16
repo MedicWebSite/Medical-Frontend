50375a255c34cb12b477ddc042c26ce0e92c2aa7
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-04-17 00:11:30 +0500

    portfolio responsive

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -11,6 +11,32 @@ const Portfolio = () => {
         slidesToShow: 4,
         slidesToScroll: 4,
         initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+
     };
     return (
         <div className="portfolio">
